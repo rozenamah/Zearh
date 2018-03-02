7ac4d103ed7e211d9a90904242d9b8f17840f8f3
Dominik Majda <ddmajda@gmail.com>
2018-03-02 15:10:20 +0100

    Initial Commit

---
+++ Rozenamah/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rozenamah
+//
+//  Created by Dominik Majda on 02.03.2018.
+//  Copyright © 2018 Dominik Majda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
