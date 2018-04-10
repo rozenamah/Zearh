e2000ef553a329288f7f146b95eeb1b015f5a550
lukaszdrozdz <drozdz93@gmail.com>
2018-04-10 10:03:40 +0200

    Transactions screen update

---
+++ Rozenamah/Scenes/Settings/TransactionHistory/Cells/ProfileTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileTableViewCell.swift
+//  Rozenamah
+//
+//  Created by Łukasz Drożdż on 09.04.2018.
+//  Copyright © 2018 Dominik Majda. All rights reserved.
+//
+
+import UIKit
+import SwiftCake
+
+class ProfileTableViewCell: UITableViewCell, SCReusableCell {
+    
+    // MARK: Outlets
+    
+    // MARK: Properties
+    
+}
---
+++ Rozenamah/Scenes/Settings/TransactionHistory/Cells/SummaryTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  SummaryTableViewCell.swift
+//  Rozenamah
+//
+//  Created by Łukasz Drożdż on 09.04.2018.
+//  Copyright © 2018 Dominik Majda. All rights reserved.
+//
+
+import UIKit
+import SwiftCake
+
+class SummaryTableViewCell: UITableViewCell, SCReusableCell {
+    
+    // MARK: Outlets
+    
+    // MARK: Properties
+    
+}
