e37c90fbab7f5ff545101154dd065f818ab7d11f
lukaszdrozdz <drozdz93@gmail.com>
2018-04-26 10:52:33 +0200

    Added scroll view for popups

--- Rozenamah/Scenes/Main/MainScreen/Patient/DoctorLocation/DoctorLocationViewController.swift
+++ Rozenamah/Scenes/Main/MainScreen/Patient/DoctorLocation/DoctorLocationViewController.swift
@@ -1,5 +1,6 @@
 import UIKit
 import CoreLocation
+import SwiftCake
 
 protocol DoctorLocationDisplayLogic: class {
     func visitCancelled()
@@ -11,11 +12,25 @@ class DoctorLocationViewController: UIViewController, DoctorLocationDisplayLogic
 
     // MARK: Outlets
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var avatarImage: SCImageView!
+    @IBOutlet weak var classificationLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var feeLabel: UILabel!
+    @IBOutlet weak var phoneNumber: UIButton!
+    @IBOutlet weak var distanceButton: UIButton!
+    
     // MARK: Properties
     var interactor: DoctorLocationBusinessLogic?
     var router: DoctorLocationRouter?
     // Delegate for updating location of doctor, and cancellation of ongoing visit
     weak var flowDelegate: PatientFlowDelegate?
+    
+    var visitInfo: VisitDetails! {
+        didSet {
+            customizeDoctorInfo()
+        }
+    }
 
     // MARK: Object lifecycle
 
@@ -40,6 +55,28 @@ class DoctorLocationViewController: UIViewController, DoctorLocationDisplayLogic
 
     fileprivate func setupView() {
         interactor?.observeDoctorLocation(for: "visitId")
+    }
+    
+    func customizeDoctorInfo() {
+        let user = visitInfo.user
+        let visit = visitInfo.visit
+        avatarImage.setAvatar(for: user)
+        nameLabel.text = user.fullname
+        priceLabel.text = "\(visit.price) SAR"
+        phoneNumber.setTitle(visit.phone ?? "No phone number", for: .normal)
+        distanceButton.setTitle("\(visit.distanceInKM) km from you", for: .normal)
+        
+        // Without this phone number will rever title to previous one (it is a bug but source is uknown)
+        phoneNumber.setTitle(visit.phone ?? "No phone number", for: .highlighted)
+        distanceButton.setTitle("\(visit.distanceInKM) km from you", for: .highlighted)
+        
+        // If more then 10 kilometers, highlight distance to red
+        distanceButton.tintColor = visit.distanceInKM > 10 ? .rmRed : .rmGray
+        
+        // If fee > 0, show fee label
+        feeLabel.isHidden = visit.fee <= 0
+        feeLabel.text = "+ \(visit.fee) SAR for cancellation"
+        
     }
 
     // MARK: Event handling
